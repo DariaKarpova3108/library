04a8c875db3139ab4dc84e21318038724a340b0a
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-10-06 17:54:48 +0300

    created tests for models Reader)

--- src/main/java/library/code/models/LibraryCard.java
+++ src/main/java/library/code/models/LibraryCard.java
@@ -1,7 +1,18 @@
 package library.code.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
